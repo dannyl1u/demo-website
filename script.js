eb1ff0f65168f1f3565405dece13180450fa280f
Danny Liu <dannyjialiliu@gmail.com>
2024-03-23 15:40:04 -0700

    hello world

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    document.querySelector('h1').textContent = 'Never going to give you up, never going to let you down!';
+});
